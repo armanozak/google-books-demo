54bd4495f1b8163fedf8f6a9ee2c18bb51621422
Arman Ozak <arman@armanozak.com>
2021-06-02 22:12:31 +0300

    initial commit

---
+++ apps/search/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'google-books-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'search';
+}
