417a2832a0d58a2387e977641ed5f500c6a5e364
Arman Ozak <arman@armanozak.com>
2021-06-03 18:55:33 +0300

    import http client module in api module

--- libs/api/src/lib/api.module.ts
+++ libs/api/src/lib/api.module.ts
@@ -1,8 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { ApiModuleConfig } from './models/config.model';
 import { GOOGLE_API_URL } from './tokens/api-url.token';
 
-@NgModule()
+@NgModule({
+  imports: [HttpClientModule],
+})
 export class ApiModule {
   static forRoot({
     baseUrl,
