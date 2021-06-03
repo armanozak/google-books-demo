844172b33dc2a5b76ad21ba7672e2bce0ef8d9d2
Arman Ozak <arman@armanozak.com>
2021-06-03 09:03:09 +0300

    replace search app prefix

--- apps/search/src/app/app.component.ts
+++ apps/search/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'google-books-root',
+  selector: 'app-root',
   template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {}
