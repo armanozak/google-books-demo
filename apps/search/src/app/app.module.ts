ffec8b78549d47c88fba009fb780c2bc81e61468
Arman Ozak <arman@armanozak.com>
2021-06-03 18:56:19 +0300

    import api module in app module

--- apps/search/src/app/app.module.ts
+++ apps/search/src/app/app.module.ts
@@ -1,12 +1,13 @@
+import { ApiModule } from '@/api';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TOOLBAR_COLOR } from './layouts/common/tokens/toolbar-color.token';
-import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +15,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    ApiModule.forRoot(),
     MatButtonModule,
     environment.production ? [] : AkitaNgDevtools.forRoot(),
   ],
