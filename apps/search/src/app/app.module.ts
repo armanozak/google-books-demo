49444f588bed5ec754172ba5912f3edf2a29c81c
Arman Ozak <arman@armanozak.com>
2021-06-03 19:42:48 +0300

    convert the app to pwa

--- apps/search/src/app/app.module.ts
+++ apps/search/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
@@ -18,6 +19,10 @@ import { TOOLBAR_COLOR } from './layouts/common/tokens/toolbar-color.token';
     ApiModule.forRoot(),
     MatButtonModule,
     environment.production ? [] : AkitaNgDevtools.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [
     {
